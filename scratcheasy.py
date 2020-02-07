b35d3fb4ad5d0026a34b4749e05f0d48cd4f5646
tanishqvyas <tanishqvyas069@gmail.com>
2020-02-07 20:34:37 +0530

    Added easy implementation using keras

---
+++ scratcheasy.py
@@ -0,0 +1,29 @@
+"""
+
+Reference Article Link : https://towardsdatascience.com/neural-networks-from-scratch-easy-vs-hard-b26ddc2e89c7
+
+"""
+
+import pandas as pd
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import RMSprop, Adadelta, Adam
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+dig = load_digits()
+onehot_target = pd.get_dummies(dig.target)
+x_train, x_val, y_train, y_val = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
+
+model = Sequential()
+model.add(Dense(128, input_dim=x_train.shape[1], activation='sigmoid'))
+model.add(Dense(128, activation='sigmoid'))
+model.add(Dense(10, activation='softmax'))
+
+print(model.summary())
+
+model.compile(optimizer=Adadelta(), loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(x_train, y_train, epochs=50, batch_size=64)
+
+scores = model.evaluate(x_val, y_val)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
